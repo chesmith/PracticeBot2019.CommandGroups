842f7c97bc9f4cd548577ff483aea1f375ab4ea2
Blanton <brooke.blanton@duchesne.org>
2019-01-27 16:15:41 -0600

    Added Hatch Mechanism

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Hatch;
 import frc.robot.subsystems.tank_drive;
 
 /**
@@ -27,6 +28,7 @@ public class Robot extends TimedRobot {
   public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
   public static OI m_oi;
   public static tank_drive kopchassis = new tank_drive();
+  public static Hatch hatch;// = new Hatch();
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
@@ -38,12 +40,21 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_oi = new OI();
+    
+    try {
+    hatch = new Hatch();
+    }
+    catch(Exception ex){
+      System.out.println("Failed Create Hatch");
+    }
 
     kopchassis.configDrive();
     
     m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
+
+
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/Hatch.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class Hatch extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  Solenoid finger = new Solenoid(1);
+  Solenoid pusher = new Solenoid(2);
+  
+  public void score(){
+    finger.set(false);
+    //TODO: we need to pause here for half of a second
+    pusher.set(true);
+  }
+
+  public void reset(){
+    pusher.set(false);
+    finger.set(true);
+  }
+
+  public void retrieve(){
+    finger.set(true);
+  }
+  
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // TODO: set default command to be reset
+
+  }
+}
